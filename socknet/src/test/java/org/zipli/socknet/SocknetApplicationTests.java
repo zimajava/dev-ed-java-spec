bdd40456cca8b2c1b2b016fe170891f95998fde7
Andrew <pikozh13@gmail.com>
2020-11-27 16:27:55 +0200

    initializing project

---
+++ socknet/src/test/java/org/zipli/socknet/SocknetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zipli.socknet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocknetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
