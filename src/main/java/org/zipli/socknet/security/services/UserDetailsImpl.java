85fa3e3118acacaf133ca18ee883e3d678064c6d
Artemiy <artemiy5555@gmail.com>
2020-11-30 16:29:47 +0200

    Test Commit

---
+++ src/main/java/org/zipli/socknet/security/services/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package org.zipli.socknet.security.services;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Objects;
+import org.zipli.socknet.model.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private User user;
+
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public UserDetailsImpl(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+	@Override
+	public String getPassword() {
+		return user.getPass();
+	}
+	@Override
+	public String getUsername() { return user.getUserName(); }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		UserDetailsImpl user = (UserDetailsImpl) o;
+		return Objects.equals(this.user.getUserName(), user.getUsername());
+	}
+}
---
+++ src/main/java/org/zipli/socknet/security/util/JwtUtils.java
@@ -0,0 +1,50 @@
+package org.zipli.socknet.security.util;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.zipli.socknet.security.services.UserDetailsImpl;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtils {
+
+	@Value("${bezkoder.app.jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${bezkoder.app.jwtExpirationMs}")
+	private int jwtExpirationMs;
+
+	public String generateJwtToken(Authentication authentication) {
+
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		return Jwts.builder()
+				.setSubject((userDetails.getUsername()))
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.compact();
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+	}
+
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException e) {
+			log.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (Exception e){
+			log.error("Error JWT: {}", e.getMessage());
+		}
+
+		return false;
+	}
+}
