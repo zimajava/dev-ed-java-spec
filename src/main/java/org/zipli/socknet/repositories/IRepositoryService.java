09d5c7130dd76c9dcbe3264a8714e072dee8cbc2
Yaroslav Semeniuk <sem.yark1@gmail.com>
2020-12-01 15:30:23 +0200

    Fix packages in project & add Connection to DB

--- src/main/java/org/zipli/socknet/repositories/IRepositoryService.java
+++ src/main/java/org/zipli/socknet/repositories/IRepositoryService.java
@@ -1,4 +1,4 @@
-package repositories;
+package org.zipli.socknet.repositories;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/org/zipli/socknet/repositories/modelsRepositories/UserRepository.java
+++ src/main/java/org/zipli/socknet/repositories/modelsRepositories/UserRepository.java
@@ -1,6 +1,6 @@
-package repositories.modelsRepositories;
+package org.zipli.socknet.repositories.modelsRepositories;
 
-import models.User;
+import org.zipli.socknet.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
