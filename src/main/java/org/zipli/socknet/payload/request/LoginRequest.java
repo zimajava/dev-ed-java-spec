78a3a25ae9dece9148df47c7fcbb6f86d73abbd9
Dariia Kaptiurova <daria.kaptiurova@gmail.com>
2020-12-09 14:05:40 +0200

    Delete realisation in LoginRequest

--- src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
+++ src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
@@ -1,11 +1,5 @@
 package org.zipli.socknet.payload.request;
 
 public class LoginRequest {
-    private String email;
-    private String password;
 
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
