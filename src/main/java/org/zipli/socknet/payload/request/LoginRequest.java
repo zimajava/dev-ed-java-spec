641a65f0101972223d7c5f1b1aabf4bbec6b3108
Eugene <jtfactum@gmail.com>
2020-12-10 22:09:24 +0200

    add LoginRequest & SignupRequest

--- src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
+++ src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
@@ -1,4 +1,25 @@
 package org.zipli.socknet.payload.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Validated
 public class LoginRequest {
+    @NotBlank(message = "Login can't be empty")
+    private String login;
+
+    @NotBlank(message = "Password can't be empty")
+    @Size(min = 8, max = 16)
+    private String password;
+
+    public LoginRequest(@NotBlank String login, @NotBlank String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
--- src/main/java/org/zipli/socknet/payload/request/SignupRequest.java
+++ src/main/java/org/zipli/socknet/payload/request/SignupRequest.java
@@ -2,14 +2,30 @@ package org.zipli.socknet.payload.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Language;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class SignupRequest {
-
+    @NotBlank(message = "Email can't be empty")
+    @Email
+    @Size(max = 50)
+    @Pattern(regexp = "")
     private String email;
+
+    @NotBlank(message = "Password can't be empty")
+    @Size(min = 8, max = 16)
     private String password;
+
+    @NotBlank(message = "User name can't be empty")
     private String userName;
+
+    @NotBlank(message = "Nickname can't be empty")
     private String nickName;
 
     public SignupRequest(String email, String password, String userName, String nickName) {
