44f8d2efe4530b19e634652305d5aea48bf66a6e
andrewpikozh <66901090+andrewpikozh@users.noreply.github.com>
2020-12-14 09:26:20 +0200

    add registration

--- src/main/java/org/zipli/socknet/exception/AuthException.java
+++ src/main/java/org/zipli/socknet/exception/AuthException.java
@@ -1,6 +1,14 @@
 package org.zipli.socknet.exception;
 
+import org.springframework.security.core.AuthenticationException;
+
 public class AuthException extends RuntimeException {
+
+    private int exceptionCode;
+
+    public AuthException(int exceptionCode) {
+        this.exceptionCode = exceptionCode;
+    }
 
     public AuthException(String message) {
         super(message);
