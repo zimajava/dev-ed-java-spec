f1728dc0d4c29f791f784f380f8e6b5101a7726c
Валентин Журавель <40039970+valek-z@users.noreply.github.com>
2020-12-16 15:42:20 +0200

    fix test

--- src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
+++ src/main/java/org/zipli/socknet/payload/request/LoginRequest.java
@@ -1,5 +1,12 @@
 package org.zipli.socknet.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    private String email;
+    private String password;
 
 }
