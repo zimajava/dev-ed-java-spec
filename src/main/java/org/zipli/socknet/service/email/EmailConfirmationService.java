138a6850def2644f48ddcb502c54e4c0ef325fa2
Zima Maksim <maksimz@wizardsdev.com>
2020-12-22 22:16:48 +0200

    update confirm mail

--- src/main/java/org/zipli/socknet/service/email/EmailConfirmationService.java
+++ src/main/java/org/zipli/socknet/service/email/EmailConfirmationService.java
@@ -3,12 +3,16 @@ package org.zipli.socknet.service.email;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.zipli.socknet.exception.NotConfirmAccountException;
 import org.zipli.socknet.model.User;
 import org.zipli.socknet.repository.UserRepository;
 import org.zipli.socknet.security.jwt.JwtUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailConfirmationService {
@@ -26,15 +30,28 @@ public class EmailConfirmationService {
     }
 
     @Async
-    public void sendEmail(String email, String token) {
+    public void sendEmail(String email, String token) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
 
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(email);
-        mailMessage.setSubject("Complete Registration!");
-        mailMessage.setFrom("zipli.socknet@gmail.com");
-        mailMessage.setText("To confirm your account, please click here : "
-                + deploy + "/zipli/auth/confirm-account?token=" + token);
-        javaMailSender.send(mailMessage);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+        helper.setTo(email);
+        helper.setSubject("Complete Registration!");
+        helper.setFrom("zipli.socknet@gmail.com");
+
+        String htmlMsg = "<h3>Confirm your mail</h3>"
+                + "<p style=\"font-size:18px;\">To confirm your account, please click "
+                + "<strong>"
+                + "<a href=\"" + deploy + "/confirm-mail?token=" + token + "\" target=\"_blank\">here</a>"
+                + "</strong>"
+                + "</p>"
+                + "<br/>"
+                + "<br/>"
+                + "<br/>"
+                +"<img src='http://www.apache.org/images/asf_logo_wide.gif'>";
+
+        message.setContent(htmlMsg, "text/html");
+
+        javaMailSender.send(message);
     }
 
     public String confirmAccount(String token) {
@@ -42,7 +59,7 @@ public class EmailConfirmationService {
         if (token != null) {
             String userName = jwtUtils.getUserNameFromJwtToken(token);
             User user = userRepository.getByUserName(userName);
-            user.isConfirm();
+            user.setConfirm(true);
             userRepository.save(user);
             return "Account verified";
         } else {
