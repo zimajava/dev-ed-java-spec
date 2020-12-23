b19d2157db5fc87a760606da5ae373a0ae2cbc57
andrewpikozh <66901090+andrewpikozh@users.noreply.github.com>
2020-12-23 15:58:12 +0200

    fix signup

--- src/main/java/org/zipli/socknet/payload/request/SignupRequest.java
+++ src/main/java/org/zipli/socknet/payload/request/SignupRequest.java
@@ -2,8 +2,6 @@ package org.zipli.socknet.payload.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Language;
-import org.springframework.lang.NonNull;
 
 import javax.validation.constraints.*;
 
