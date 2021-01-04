9c940b083088654b26d84f5cee1870080d93021c
andrewpikozh <66901090+andrewpikozh@users.noreply.github.com>
2021-01-04 16:25:36 +0200

    fix problems

--- src/main/java/org/zipli/socknet/dto/WsMessage.java
+++ src/main/java/org/zipli/socknet/dto/WsMessage.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class WsMessage {
+public class WsMessage  {
     private Command command;
     private BaseData data;
 
