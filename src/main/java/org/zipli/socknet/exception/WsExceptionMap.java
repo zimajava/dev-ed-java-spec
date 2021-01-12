4cef432ddfd3504852a3909fa1826cb6562440e7
Artemiy <artemiy5555@gmail.com>
2021-01-12 10:30:12 +0200

    Add error map for WS and fix exception in MessageService. Refactoring Exception.

--- src/main/java/org/zipli/socknet/exception/WsExceptionMap.java
+++ src/main/java/org/zipli/socknet/exception/WsExceptionMap.java
@@ -1,13 +1,13 @@
 package org.zipli.socknet.exception;
 
 public enum WsExceptionMap {
-    CHAT_ACCESS_ERROR(21),
-    MESSAGE_ACCESS_ERROR(22),
-    ALREADY_EXISTS(23),
-    CHAT_NOT_EXIT(24),
-    MESSAGE_NOT_EXIT(25),
-    UNEXPECTED_EXCEPTION(26),
-    USER_NOT_FOUND_EXCEPTION(27);
+    CHAT_ACCESS_ERROR(11),
+    MESSAGE_ACCESS_ERROR(12),
+    ALREADY_EXISTS(13),
+    CHAT_NOT_EXIT(14),
+    MESSAGE_NOT_EXIT(15),
+    UNEXPECTED_EXCEPTION(16),
+    USER_NOT_FOUND_EXCEPTION(17);
 
     private final long numberException;
 
