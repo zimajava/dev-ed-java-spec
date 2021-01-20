8e2b7d3f1ebfa3c3c12c1b1e250c90f1109d663c
Yaroslav Semeniuk <sem.yark1@gmail.com>
2021-01-21 00:21:09 +0200

    add FullUserInfo

---
+++ src/main/java/org/zipli/socknet/dto/response/FullUserInfo.java
@@ -0,0 +1,25 @@
+package org.zipli.socknet.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.zipli.socknet.model.User;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FullUserInfo extends UserInfo{
+    private List<String> chatsId;
+
+    public FullUserInfo(User user) {
+        super(user.getEmail(), user.getId(),user.getUserName(), user.getNickName(), user.getAvatar());
+        this.chatsId = user.getChatsId();
+    }
+
+    public FullUserInfo(List<String> chatsId) {
+        this.chatsId = chatsId;
+    }
+
+}
