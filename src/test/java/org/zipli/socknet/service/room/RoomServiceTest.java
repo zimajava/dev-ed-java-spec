0703ad455df125b30cd8b549a0a8bee670381760
Artemiy <artemiy5555@gmail.com>
2021-01-20 10:31:54 +0200

    fix code

---
+++ src/test/java/org/zipli/socknet/service/room/RoomServiceTest.java
@@ -0,0 +1,67 @@
+package org.zipli.socknet.service.room;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@DataMongoTest
+class RoomServiceTest {
+
+    @Autowired
+    RoomService roomService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getRoom() {
+
+    }
+
+    @Test
+    void getRooms() {
+
+    }
+
+    @Test
+    void joinRoom() {
+
+    }
+
+    @Test
+    void leaveRoom() {
+
+    }
+
+    @Test
+    void createRoom() {
+
+    }
+
+    @Test
+    void saveMessage() {
+
+    }
+
+    @Test
+    void getMessagesByRoom() {
+
+    }
+
+    @Test
+    void subscribeMessage() {
+
+    }
+
+    @Test
+    void deleteRoom() {
+
+    }
+}
